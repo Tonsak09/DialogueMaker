bb3239b8a7648410fbeaedbbb5e83b9ecd02645a
Narai Risser <43816475+Tonsak09@users.noreply.github.com>
2024-02-07 13:32:31 -0500

    Trying to parse text

--- DialogueForms/Form1.Designer.cs
+++ DialogueForms/Form1.Designer.cs
@@ -30,30 +30,36 @@
         {
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.insert = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.Dialogue = new System.Windows.Forms.ListBox();
+            this.insertDropDown = new System.Windows.Forms.ComboBox();
+            this.insertText = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.Display = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.RawText = new System.Windows.Forms.RichTextBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.DisplayText = new System.Windows.Forms.RichTextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.Display.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.insert);
             this.groupBox1.Controls.Add(this.button5);
             this.groupBox1.Controls.Add(this.button4);
             this.groupBox1.Controls.Add(this.Dialogue);
-            this.groupBox1.Controls.Add(this.comboBox1);
-            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.insertDropDown);
+            this.groupBox1.Controls.Add(this.insertText);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Location = new System.Drawing.Point(12, 38);
             this.groupBox1.Name = "groupBox1";
@@ -61,6 +67,34 @@
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Controls";
+            // 
+            // insert
+            // 
+            this.insert.Location = new System.Drawing.Point(153, 33);
+            this.insert.Name = "insert";
+            this.insert.Size = new System.Drawing.Size(75, 23);
+            this.insert.TabIndex = 9;
+            this.insert.Text = "Insert";
+            this.insert.UseVisualStyleBackColor = true;
+            this.insert.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(493, 43);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Down";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(493, 14);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Up";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Dialogue
             // 
@@ -74,6 +108,35 @@
             this.Dialogue.Name = "Dialogue";
             this.Dialogue.Size = new System.Drawing.Size(156, 148);
             this.Dialogue.TabIndex = 6;
+            // 
+            // insertDropDown
+            // 
+            this.insertDropDown.FormattingEnabled = true;
+            this.insertDropDown.Items.AddRange(new object[] {
+            "Shake",
+            "Large",
+            "Wiggle"});
+            this.insertDropDown.Location = new System.Drawing.Point(16, 33);
+            this.insertDropDown.Name = "insertDropDown";
+            this.insertDropDown.Size = new System.Drawing.Size(121, 24);
+            this.insertDropDown.TabIndex = 3;
+            // 
+            // insertText
+            // 
+            this.insertText.Location = new System.Drawing.Point(16, 63);
+            this.insertText.Name = "insertText";
+            this.insertText.Size = new System.Drawing.Size(276, 96);
+            this.insertText.TabIndex = 2;
+            this.insertText.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
@@ -92,35 +155,6 @@
             this.button2.TabIndex = 4;
             this.button2.Text = "Load";
             this.button2.UseVisualStyleBackColor = true;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Shake",
-            "Large",
-            "Wiggle"});
-            this.comboBox1.Location = new System.Drawing.Point(16, 33);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 3;
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(16, 63);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(276, 96);
-            this.richTextBox1.TabIndex = 2;
-            this.richTextBox1.Text = "";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(16, 165);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Insert";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // groupBox2
             // 
@@ -141,9 +175,11 @@
             this.Display.SelectedIndex = 0;
             this.Display.Size = new System.Drawing.Size(723, 160);
             this.Display.TabIndex = 1;
+            this.Display.Click += new System.EventHandler(this.Display_Click);
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.RawText);
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -152,8 +188,17 @@
             this.tabPage1.Text = "RawText";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // RawText
+            // 
+            this.RawText.Location = new System.Drawing.Point(7, 7);
+            this.RawText.Name = "RawText";
+            this.RawText.Size = new System.Drawing.Size(702, 118);
+            this.RawText.TabIndex = 0;
+            this.RawText.Text = "";
+            // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.DisplayText);
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
@@ -162,23 +207,13 @@
             this.tabPage2.Text = "DisplayText";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // DisplayText
             // 
-            this.button4.Location = new System.Drawing.Point(493, 14);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 7;
-            this.button4.Text = "Up";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(493, 43);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 8;
-            this.button5.Text = "Down";
-            this.button5.UseVisualStyleBackColor = true;
+            this.DisplayText.Location = new System.Drawing.Point(6, 6);
+            this.DisplayText.Name = "DisplayText";
+            this.DisplayText.Size = new System.Drawing.Size(702, 118);
+            this.DisplayText.TabIndex = 1;
+            this.DisplayText.Text = "";
             // 
             // Form1
             // 
@@ -194,6 +229,8 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             this.Display.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -204,8 +241,8 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RichTextBox insertText;
+        private System.Windows.Forms.ComboBox insertDropDown;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ListBox Dialogue;
@@ -214,6 +251,9 @@
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button insert;
+        private System.Windows.Forms.RichTextBox RawText;
+        private System.Windows.Forms.RichTextBox DisplayText;
     }
 }
 
--- DialogueForms/Form1.cs
+++ DialogueForms/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -18,5 +19,46 @@ namespace DialogueForms
             InitializeComponent();
         }
 
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (insertDropDown.Text == "")
+                return;
+            
+            RawText.Text += $"<{insertDropDown.Text}>({insertText.Text})";
+        }
+
+        private void Display_Click(object sender, EventArgs e)
+        {
+            DisplayText.Text = ParseOutFX(RawText.Text);
+        }
+
+        private string ParseOutFX(string text)
+        {
+            string parsedText = "";
+
+            Regex regex = new Regex(@"<\w+>\(\w+\)");
+            Regex textRX = new Regex(@"[\w\s]*");
+
+            string[] textNoFX = regex.Split(text);
+            MatchCollection matches = regex.Matches(text);
+            MatchCollection textExtracts;
+
+            int counter = 0;
+            foreach (Match match in matches)
+            {
+                textExtracts = textRX.Matches(match.Value);
+                parsedText += textNoFX[counter] + textExtracts[0];
+
+                counter++;
+                // if (textNoFX[counter] != "")
+            }
+
+            for(int i = 0; i < (textNoFX.Length - counter); i++)
+            {
+                parsedText += textNoFX[counter + i];
+            }
+
+            return parsedText;
+        }
     }
 }
